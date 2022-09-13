5916c7bfe5885f45945898f3c77742aa8dfcc8e3
dionesde <diones.de@redes.ufsm.br>
2022-09-12 22:09:38 -0300

    Update SimplePanel.tsx

--- grafana-path/src/SimplePanel.tsx
+++ grafana-path/src/SimplePanel.tsx
@@ -30,6 +30,7 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
             Number of series: {data.series.length}
           </div>
       )}
+      <div>{data.series.toString()}</div>
       <div>Text option value: {options.text}</div>
     </div>
       <RLMap
