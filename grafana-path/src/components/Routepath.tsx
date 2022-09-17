7381fa2a88da5d272123f21afcd778a2a0698c86
dionesde <diones.de@redes.ufsm.br>
2022-09-17 18:49:07 -0300

    update

--- grafana-path/src/components/Routepath.tsx
+++ grafana-path/src/components/Routepath.tsx
@@ -18,7 +18,7 @@ function RoutePath(props: RoutePathProps): ReactElement {
                 center={[point[0],point[1]]}
                 key={`${round(point[0], 1)},${round(point[1], 1)}`}
                 pathOptions={{ color: 'red'}}
-                radius={radius ? radius : 40}>
+                radius={radius ? radius : 4}>
             </CircleMarker>
             )
         )}
