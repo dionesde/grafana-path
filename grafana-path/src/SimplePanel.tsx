59b660c781aae7ea155a64bff057827c0c7896dd
dionesde <diones.de@redes.ufsm.br>
2022-09-21 01:30:20 -0300

    update

--- grafana-path/src/SimplePanel.tsx
+++ grafana-path/src/SimplePanel.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {DataFrame, PanelProps} from '@grafana/data';
 import { SimpleOptions } from 'types';
-import { css} from 'emotion';
-import { stylesFactory, useTheme } from '@grafana/ui';
+import {useTheme } from '@grafana/ui';
 import { Map as RLMap, TileLayer} from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import './painel.css';
@@ -16,27 +15,7 @@ interface Props extends PanelProps<SimpleOptions> {}
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   const theme = useTheme();
-  //const styles = getStyles();
   return (
-   /* <div
-      className={cx(
-        styles.wrapper,
-        css`
-          width: ${width}px;
-          height: ${height}px;
-        `
-      )}
-    > <div className={styles.textBox}>
-      {options.showSeriesCount && (
-          <div
-              className={css`
-              font-size: ${theme.typography.size[options.seriesCountSize]};
-            `}
-          >
-            Number of series: {data.series.length}
-          </div>
-      )}
-    </div>*/
       <RLMap
         center={[options.lat, options.lng]}
         zoom={options.zoom}
@@ -70,21 +49,3 @@ function processData( series: DataFrame[]): any{
   return entries;
 }
 
-const getStyles = stylesFactory(() => {
-  return {
-    wrapper: css`
-      position: relative;
-    `,
-    svg: css`
-      position: absolute;
-      top: 0;
-      left: 0;
-    `,
-    textBox: css`
-      position: relative;
-      top: 0;
-      left: 0;
-      padding: 10px;
-    `,
-  };
-});
