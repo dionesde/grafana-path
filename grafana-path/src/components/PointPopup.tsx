37dd009d829c522b74a8ed4f5219ba952e731513
dionesde <diones.de@redes.ufsm.br>
2022-10-18 10:52:16 -0300

    update

--- grafana-path/src/components/PointPopup.tsx
+++ grafana-path/src/components/PointPopup.tsx
@@ -25,7 +25,7 @@ const PointPopup: React.FC<PointPopupProps> = ({ description }) => {
     );
     function check_pop(description: any): any{
         if(description){
-            return <Popup autoClose={true} className="point-popup">
+            return <Popup onClose={()=>{alert('teste')}} className="point-popup">
                 { parse(description ? description : '') }
             </Popup>
         }
